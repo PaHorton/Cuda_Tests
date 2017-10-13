00dc9756d4022a9e6532fbfc5e2817c6e1f5d4ba
Paul Horton <giansar26@gmail.com>
2017-10-13 11:08:54 -0700

    Array multi

---
+++ Array_Multi/array.cu
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+const int N = 16;
+const int blocksize = 16;
+
+__global__
+void multi(int *a, int *b)
+{
+	a[threadIdx.x] *= b[threadIdx.x];
+}
+
+int main()
+{
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    int b[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+    int *ad;
+    int *bd;
+
+    const int isize = N*sizeof(int);
+
+    for(int i = 0; i < N; i++){
+        printf("%d ",a[i]);
+    }
+
+    cudaMalloc( (void**)&ad, isize );
+	cudaMalloc( (void**)&bd, isize );
+
+    cudaMemcpy( ad, a, isize, cudaMemcpyHostToDevice );
+    cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice );
+
+    dim3 dimBlock( blocksize, 1 );
+    dim3 dimGrid( 1, 1 );
+    multi<<<dimGrid, dimBlock>>>(ad, bd);
+
+    cudaMemcpy( a, ad, isize, cudaMemcpyDeviceToHost );
+    cudaFree( ad );
+    cudaFree( bd );
+
+    for(int i = 0; i < N; i++){
+        printf("%d ",a[i]);
+    }
+}
